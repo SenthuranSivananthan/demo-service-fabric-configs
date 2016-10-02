d243c01547d7aaf5b601259a2fa1b7e31a976986
Senthuran Sivananthan [MSFT] <Senthuran.Sivananthan@microsoft.com>
2016-10-02 09:00:57 -0400

    Scaffolded project

---
+++ ConfigOptions/ConfigurationApi/Controllers/ValuesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace ConfigurationApi.Controllers
+{
+    [ServiceRequestActionFilter]
+    public class ValuesController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
